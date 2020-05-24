d4277a5d011e5f9abe62e6d527a634cf45ea904e
Christopher C. Odom <christopher.odom@knights.ucf.edu>
2020-05-23 20:04:10 -0400

    theory of T&T class stuff

---
+++ CHRISTOPHER_ODOM_Tracery_Grammar.js
@@ -0,0 +1,59 @@
+{
+	"origin": [
+		"The rhetoric of the image may #verb1# #pluralnoun1# to #verb2# #pluralnoun2#."
+	],
+	"verb1": [
+		"articulate",
+		"present",
+		"assemble",
+		"utilize",
+		"communicate",
+		"orientate",
+		"elucidate",
+		"canalize",
+		"naturalize",
+		"compose"
+	],
+	"pluralnoun1": [
+		"linguistics",
+		"text",
+		"systems",
+		"signifiers",
+		"lexias",
+		"lexicons",
+		"ideolects",
+		"syntagms",
+		"denotations",
+		"signs",
+		"metalanguages",
+		"literal messages",
+		"signifieds"
+	],
+	"verb2": [
+		"relay",
+		"produce",
+		"describe",
+		"define",
+		"imbue",
+		"represent",
+		"imprint",
+		"interpret",
+		"construct",
+		"transmit"
+	],
+	"pluralnoun2": [
+		"symbols",
+		"meanings",
+		"polysemies",
+		"codes",
+		"ideologies",
+		"connotators",
+		"paradigms",
+		"images",
+		"connotations",
+		"messages",
+		"coded messages",
+		"linguistic messages",
+		"anchorage"
+	]
+}
--- scripts.js
+++ scripts.js
@@ -1,22 +1,22 @@
-let arraySymbolicMessage = [["id", 1], ["term", "term"], ["definition", "definition"]];
+let arraySymbolicMessage = [["id", 1], ["term", "symbolic message"], ["definition", "a connoted message created with the image"]];
 
-let arrayConnotedImage = [["id", 2], ["term", "term"], ["definition", "definition"]];
+let arrayConnotedImage = [["id", 2], ["term", "connoted image"], ["definition", "a symbolic message whereby a sign metaphorically represents the signified"]];
 
-let arrayDenotedImage = [["id", 3], ["term", "term"], ["definition", "definition"]];
+let arrayDenotedImage = [["id", 3], ["term", "denoted image"], ["definition", "the literal representation of an image"]];
 
-let arrayAnchorage = [["id", 4], ["term", "term"], ["definition", "definition"]];
+let arrayAnchorage = [["id", 4], ["term", "anchorage"], ["definition", "utilizes text to focus the possible meanings of an image"]];
 
-let arrayRelay = [["id", 5], ["term", "term"], ["definition", "definition"]];
+let arrayRelay = [["id", 5], ["term", "relay"], ["definition", "utilizes text to add meaning in conjunction with the image to convey a larger message"]];
 
-let arrayPolysemy = [["id", 6], ["term", "term"], ["definition", "definition"]];
+let arrayPolysemy = [["id", 6], ["term", "polysemy"], ["definition", "all images as signs are capable of having multiple meanings as signifieds"]];
 
-let arrayLexia = [["id", 7], ["term", "term"], ["definition", "definition"]];
+let arrayLexia = [["id", 7], ["term", "lexia"], ["definition", "the number of meanings an image possesses"]];
 
-let arraySyntagm = [["id", 8], ["term", "term"], ["definition", "definition"]];
+let arraySyntagmatic = [["id", 8], ["term", "syntagmatic"], ["definition", "an image relationship based upon the combination possibilities"]];
 
-let arrayParadigm = [["id", 9], ["term", "term"], ["definition", "definition"]];
+let arrayParadigmatic = [["id", 9], ["term", "paradigmatic"], ["definition", "an image relationship based upon functional contrast and differentiation"]];
 
-let arrayRhetoricOfTheImage = [["id", 10], ["term", "term"], ["definition", "definition"]];
+let arrayRhetoricOfTheImage = [["id", 10], ["term", "rhetoric of the image"], ["definition", "the cohesive message of the collective set of connotators in an image"]];
 
 $("#response").html(arrayRhetoricOfTheImage[1][1]);
 
